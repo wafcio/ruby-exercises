cb344c6732cf1a08dc0fa9b5d0bf862f63790174
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2023-08-06 13:07:32 +0200

    fix typo

--- spec/euler/euler_0001/euler_0001_spec.rb
+++ spec/euler/euler_0001/euler_0001_spec.rb
@@ -2,7 +2,7 @@ require_relative "../../../exercises/euler/euler_0001/euler_0001"
 
 RSpec.describe Euler1 do
   describe "Euler #1 problem" do
-    it "returns valid git" do
+    it "returns valid number" do
       expect(subject.run).to eq(233_168)
     end
   end
